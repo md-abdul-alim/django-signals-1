798788b40f7adb4f6d0d8e6f519737799fad44cc
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-10-03 19:57:54 +0600

    4 apps created
    orders, sales, buyers, cars apps created

--- django_signals_1/settings.py
+++ django_signals_1/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'buyers',
+    'cars',
+    'orders',
+    'sales'
 ]
 
 MIDDLEWARE = [
